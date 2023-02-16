17043c6a2be5cffd2cd4d880bf6c92a9031b7185
nadim-chowdhury <mrnadimc9@gmail.com>
2023-02-16 23:35:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let mainImg = document.getElementById("big-img");
+
+function changeImg1() {
+  mainImg.src =
+    "https://images.pexels.com/photos/601174/pexels-photo-601174.jpeg";
+}
+
+function changeImg2() {
+  mainImg.src =
+    "https://images.pexels.com/photos/698275/pexels-photo-698275.jpeg";
+}
+
+function changeImg3() {
+  mainImg.src =
+    "https://images.pexels.com/photos/3493777/pexels-photo-3493777.jpeg";
+}
+
+function changeImg4() {
+  mainImg.src =
+    "https://images.pexels.com/photos/62600/pexels-photo-62600.jpeg";
+}
